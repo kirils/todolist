b61aaff9c0264d15d7eefd238a68960f8ac467fe
Kiril Stoilov <kiril.stoilov@gmail.com>
2019-04-29 23:41:20 +0300

    Add Todo functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   
   </div>
@@ -9,12 +10,14 @@
 <script>
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 export default {
   name: 'app',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data () {
     return {
@@ -40,6 +43,9 @@ export default {
   methods: {
     deleteTodo (id) {
       this.todos = this.todos.filter(todo => todo.id != id)
+    },
+    addTodo(newTodo) {
+      this.todos = [... this.todos, newTodo];
     }
   }
 }
@@ -54,5 +60,17 @@ export default {
     body {
       font-family: Arial, Helvetica, sans-serif;
       line-height: 1.4;
+    }
+
+    .btn {
+      display: inline-block;
+      border: none;
+      background: #555;
+      color: #fff;
+      padding: 7px 20px;
+      cursor: pointer;
+    }
+    .btn:hover {
+      background: #666;
     }
 </style>
