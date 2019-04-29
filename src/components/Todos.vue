67fef268723bb58aadaf0e5160ce36f0f42674d4
Kiril Stoilov <kiril.stoilov@gmail.com>
2019-04-29 22:06:40 +0300

    Initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h1>{{todo.title}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
+
